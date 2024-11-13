97341294e47803eb81737d706b6aa44fd55fb496
Dhananjay <dhananjaysable370@gmail.com>
2024-11-13 17:20:01 +0530

    update user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,26 +1,28 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, model } from "mongoose";
 
-const userSchema = new mongoose.Schema(
+const userSchema = new Schema(
   {
     username: {
       type: String,
-      required: true,
+      required: [true, "Username is required"],
       unique: true,
       trim: true,
     },
     email: {
       type: String,
-      required: true,
+      required: [true, "Email is required"],
       lowercase: true,
       trim: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})$/, "Invalid email"],
     },
     password: {
       type: String,
-      required: true,
+      required: [true, "Password is required"],
       trim: true,
+      minlength: [8, "Password must be at least 8 characters"],
     },
   },
   { timestamps: true }
 );
 
-export const User = mongoose.model("User", userSchema);
+export const User = model("User", userSchema);
