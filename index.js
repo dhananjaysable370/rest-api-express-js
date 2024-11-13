dcd8b757ba1c48da6850ca3abfb00a4193109032
Dhananjay <dhananjaysable370@gmail.com>
2024-11-13 15:00:49 +0530

    setup

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { dbConnection } from "./lib/db.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const MongoUri = process.env.MONGODB_URI;
+
+dbConnection(MongoUri)
+  .then(() => {
+    console.log("DB Connected!");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
