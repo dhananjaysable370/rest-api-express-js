c1cd3be524c5fd6657f0843585f269063cf82b76
Dhananjay <dhananjaysable370@gmail.com>
2024-11-14 16:50:45 +0530

    dashboard ejs

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,20 +1,20 @@
 import { Router } from "express";
 import {
-    allUsers,
-    create,
-    getOneUser,
-    updateUser,
-    deleteUser
+  allUsers,
+  create,
+  getOneUser,
+  updateUser,
+  deleteUser,
 } from "../controllers/userController.js";
 
 const userRouter = Router();
-userRouter.get('/user', (req, res) => {
-    res.render('dashboard.ejs')
-})
+userRouter.get("/user", (req, res) => {
+  res.render("dashboard.ejs", { title: "Hello,", user:"Guest" });
+});
 userRouter.get("/allusers", allUsers);
 userRouter.post("/users", create);
 userRouter.get("/user/:id", getOneUser);
-userRouter.put('/updateuser/:id', updateUser);
-userRouter.delete('/deleteuser/:id', deleteUser);
+userRouter.put("/updateuser/:id", updateUser);
+userRouter.delete("/deleteuser/:id", deleteUser);
 
 export default userRouter;
